dfdb7e23b56d45b3881944ee100af8b40e00192c
Bonnie <bonnieacuna@gmail.com>
2019-03-23 11:35:26 -0700

    first draft

---
+++ client/build/precache-manifest.0635ea339b3a7ad8f2a894da79bdc672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f87ed2454745495f293",
+    "url": "/static/js/main.0d5ce557.chunk.js"
+  },
+  {
+    "revision": "92d5e2b18120d0b215e3",
+    "url": "/static/js/2.5097bbda.chunk.js"
+  },
+  {
+    "revision": "4f87ed2454745495f293",
+    "url": "/static/css/main.c09cf97f.chunk.css"
+  },
+  {
+    "revision": "341b252b110e4a2a05c6d247c9601d83",
+    "url": "/index.html"
+  }
+];
---
+++ models/book.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    
+    },
+    authors: {
+        type: String,
+        required: true
+    }, 
+    description: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+    },
+    link: {
+        type: String,
+    }
+});
+
+const Book = mongoose.model("Book", BookSchema);
+module.exports = Book;

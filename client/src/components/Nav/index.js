38cf5d8f98b8d3909726b9dabe72a0b50a746fd3
Bonnie <bonnieacuna@gmail.com>
2019-03-24 13:04:51 -0700

    updated Navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,13 +5,13 @@ function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="row">
-                <h1 className="navbar-brand ml-3">Google Books</h1>
+                <div className="navbar-brand ml-3">Google Books</div>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <Link to="/">Search</Link>
+                        <Link to="/">Search/</Link>
                         <Link to="/saved">Saved</Link>
                     </div>
                 </div>
